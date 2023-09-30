4074c16313c1379fb09d5994c82b92d4a3afbde7
ahmedawwan <ahmedsaeed0973@gmail.com>
2023-10-01 00:02:36 +0500

    auth completed

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
-
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 # Home View
 def home(request):
@@ -16,8 +17,34 @@ def register(request):
 
         if form.is_valid():
             form.save()
-            # return redirect('')
+            return redirect('login')
 
     context = {'form': form}
 
     return render(request, 'webapp/register.html', context)
+
+# Login View
+def login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data = request.POST)
+
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('')
+    
+    context = {"form": form}
+    return render(request, 'webapp/login.html', context)
+
+# Logout View
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
+
