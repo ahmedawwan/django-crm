a616548cc7e92cb480c971125e01700513f2c2b2
ahmedawwan <ahmedsaeed0973@gmail.com>
2023-09-30 23:37:01 +0500

    Register Completed

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
+
+# Home View
 def home(request):
     # return HttpResponse("Hello World!")
     return render(request, 'webapp/index.html')
+
+# Register View
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+
+    context = {'form': form}
+
+    return render(request, 'webapp/register.html', context)
